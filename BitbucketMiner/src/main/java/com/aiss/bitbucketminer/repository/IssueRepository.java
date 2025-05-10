880e80a1363a2866ae609caa6a6d0c8209a82052
Carlos C. <147752773+Carcan05@users.noreply.github.com>
2025-05-10 14:15:37 +0200

    Delete BitbucketMiner directory

--- BitbucketMiner/src/main/java/com/aiss/bitbucketminer/repository/IssueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aiss.bitbucketminer.repository;
-
-import com.aiss.bitbucketminer.entity.IssueEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssueRepository extends JpaRepository<IssueEntity, String> {}
-
